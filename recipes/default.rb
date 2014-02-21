6bc55ae29309a025ee21573d12d1c7af0508f0a0
Trey Hyde <thyde@centraldesktop.com>
2014-02-20 17:41:35 -0800

    Install basic packages

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,13 @@
 # 
 # All rights reserved - Do Not Redistribute
 #
+
+
+apt_repository 'elasticsearch' do
+  uri        'http://packages.elasticsearch.org/elasticsearch/1.0/debian'
+  components ['main', 'stable']
+end
+
+package "elasticsearch" do
+  action :upgrade # see actions section below
+end
