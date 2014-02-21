28d7efbc35f4a3200abcf02b63b4c5ecafa6f55b
Trey Hyde <thyde@centraldesktop.com>
2014-02-20 18:58:39 -0800

    Spelling

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe 'elastisearch::jdk'
+include_recipe 'elasticsearch::jdk'
 
 apt_repository 'elasticsearch' do
   uri        'http://packages.elasticsearch.org/elasticsearch/1.0/debian'
