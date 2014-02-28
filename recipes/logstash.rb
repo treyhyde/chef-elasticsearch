8e4f03a35ec31396264c55becb4ec9b144042124
Trey Hyde <thyde@centraldesktop.com>
2014-02-27 17:58:11 -0800

    Add config files, start service

--- recipes/logstash.rb
+++ recipes/logstash.rb
@@ -9,3 +9,25 @@ package "logstash" do
   action :upgrade # see actions section below
 end
 
+
+cookbook_file "/etc/logstash/conf.d/from_cd.conf" do
+  source from_cd.conf 
+  action :create
+  owner 'root'
+  group 'root'
+  mode 0444
+end
+
+cookbook_file "/etc/logstash/conf.d/local_machine.conf" do
+  source local_machine.conf
+  action :create
+  owner 'root'
+  group 'root'
+  mode 0444
+end
+
+
+service "logstash" do
+  action [ :enable, :start ]
+end
+
