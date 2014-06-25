3d2f0356e91cb5f7697e4cca6b6a193d53ab2005
Trey Hyde <thyde@centraldesktop.com>
2014-06-25 10:52:41 -0700

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rhyde@hydenetworks.com'
 license          'APACHE-2.0'
 description      'Installs/Configures elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.7'
+version          '0.1.8'
 
 
 depends "apt"
